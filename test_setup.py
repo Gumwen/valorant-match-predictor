d14f47145eb000253788c72f858dc71b9c8e2124
Osamwengumwenro Oni-Ojo <osamwengumwenroo@gmail.com>
2026-07-07 03:26:45 -0500

    Set up python environment and dependenciew

---
+++ test_setup.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import requests
+
+print("Valorant Match Predictor environment is ready!")
+print("Pandas version:", pd.__version__)
+print("NumPy version:", np.__version__)
